e79bd0f1ee3da107bba1100aefb5ee4e3380e02a
ArtemLyah <asuskonotopo@gmail.com>
2022-11-01 19:30:01 +0200

    debug

--- handlers/errors/errors.py
+++ handlers/errors/errors.py
@@ -7,7 +7,9 @@ import logging
 @dp.errors_handler()
 async def error_handler(update:types.Update, exception:Exception):
     if isinstance(exception, InterfaceError):
+        await bot.send_message(father_id, "Reconnecting...")
         db.connect()
+        await bot.send_message(father_id, "Reconnecting has done")
     else:
         await bot.send_message(father_id, str(exception))
         logging.exception(str(exception))
