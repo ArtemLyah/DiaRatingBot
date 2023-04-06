2bd3ca3a881ed19d7687819dab75d99df98529c2
ArtemLyah <asuskonotopo@gmail.com>
2023-04-06 14:49:28 +0300

    change config

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ db_settings = {
     "username":os.getenv("DB_USER"),
     "password":os.getenv("DB_PASSWORD"),
     "host":os.getenv("DB_HOST"),
-    "port":5432,
+    "port":os.getenv("DB_PORT"),
     "database":os.getenv("DB_NAME")
 }
 
